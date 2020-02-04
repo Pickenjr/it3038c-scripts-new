985afa3909082ae3d8a83b166c46cdaf6b409a45
Pickenjr <Pickenjr@mail.uc.edu>
2020-02-04 18:50:50 -0500

    pleasework

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,6 @@
+﻿function getIP{
+        (Get-NetIPAddress).ipv4address | Select-String "192*"
+}
+$IP = getIP
+Write-Host("This machine's IP is $IP")
+Write-Host("This machine's IP is {0}" -f $IP)
