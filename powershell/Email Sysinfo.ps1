ca6b8bc83b63efa361b15ff705ab261c14a43ada
Pickenjr <Pickenjr@mail.uc.edu>
2020-02-09 13:12:41 -0500

    Project 1

---
+++ powershell/Email Sysinfo.ps1
@@ -0,0 +1,11 @@
+﻿function getIP{
+        (Get-NetIPAddress).ipv4address | Select-String "192*"
+}
+$IP = getIP
+$User = $env:USERNAME
+$Comp = $env:COMPUTERNAME
+$Vers = $HOST.Version.Major
+$Today = Get-Date -DisplayHint Date
+$BODY = "This machine's IP is $IP. User is $User. Hostname is $Comp. Powershell version $Vers. Today's Date is $Today"
+Send-MailMessage -To "Pickenjr@mail.uc.edu" -From "pickenjr@mail.uc.edu" -Subject "IT3038c Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
+
---
+++ powershell/Project1.ps1
@@ -0,0 +1,7 @@
+﻿#This script displays the used and free space of all drives on the system
+#Line 4 assigns a variable gets our drive information, Line 5 displays the Device ID which tells us which drive is which, 
+#Lines 6 and 7 format the output of the used and free space and the header for them to show it's in GB  
+Get-WmiObject -Class win32_logicaldisk | 
+Select-Object DeviceId, 
+@{n="Size(GB)";e={[math]::Truncate($_.Size/1GB)}},
+@{n="Free Space(GB)";e={[math]::Truncate($_.FreeSpace/1GB)}}
