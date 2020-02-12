99493f6e7d92f1d5194a41434703caca929bfd2a
Pickenjr <Pickenjr@mail.uc.edu>
2020-02-11 19:16:18 -0500

    Python Scripts

---
+++ python/sockettest.py
@@ -0,0 +1,13 @@
+import socket, sys
+
+#hosts = ["www.uc.edu" , "www.google.com" , "www.bing.com"]
+#print("Working from host: "+ socket.fqdn())
+#for h in hosts:
+#   print(h + ": "+ socket.gethostbyname(h))
+
+try:
+    hostname = str(sys.argv[1])
+    ip = socket.gethostbyname(hostname)
+    print(hostname + " has an IP of "+ ip)
+except:
+    print("Oops, something is wrong with that host.")
