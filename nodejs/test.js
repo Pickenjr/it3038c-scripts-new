16d0ba046bc09fd4f24240b2923d3f69a9977615
Pickenjr <Pickenjr@mail.uc.edu>
2020-02-18 18:56:15 -0500

    Adding NodeJs

---
+++ nodejs/test.js
@@ -0,0 +1,12 @@
+var path = require("path");
+
+//var hello = "Hello from node JS Variable!";
+//console.log(`Printing the variable hello: ${hello}`);
+
+//console.log("directory name: " + __dirname);
+//console.log("directory and file name: " + __filename);
+
+//console.log("Using PATH module: ")
+//console.log(`Hello from file ${path.basename(__filename)}`);
+
+console.log(`Process args: ${process.argv}`);
