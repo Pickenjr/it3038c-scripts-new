b8e58706ff4c39cc0c68185f252a32145285f5b1
Pickenjr <Pickenjr@mail.uc.edu>
2020-03-09 21:56:48 -0400

    Lab 7 edit

--- powershell/Lab 7/Lab7.ps1
+++ powershell/Lab 7/Lab7.ps1
@@ -1,8 +1,9 @@
-﻿Find-Package x7Zip
+﻿#Find-Package x7Zip
 
-#Save-Package x7Zip
+#Mkdir C:\x7Zip
+#Save-Package x7Zip -Path C:\x7Zip
 
-Install-Package x7Zip
+#Install-Package x7Zip
 
-
+#Remove-Item C:\x7Zip -force
 #Uninstall-Package x7Zip 
